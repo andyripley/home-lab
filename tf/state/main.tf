844a6be46ad95a41c161478a2da5100753267b35
Andy Ripley <andy@ley.rip>
2026-05-08 22:18:11 -0400

    restructure in progress 2

---
+++ tf/state/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "ripley-homelab-state"
+  tags = {
+    Name        = "ripley-homelab-state"
+  }
+}
+
